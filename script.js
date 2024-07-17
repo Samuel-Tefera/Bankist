5d94f3fa7c0e977fe3e2e858d4db8dc56c715591
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-17 15:03:56 +0300

    add reveal effect on each webpage sections

--- script.js
+++ script.js
@@ -125,3 +125,24 @@ const headerObserver = new IntersectionObserver( stickyNav, {
 } );
 
 headerObserver.observe( header );
+
+// Reveal sections
+const allSection = document.querySelectorAll( '.section' );
+const revealSection = function ( entries, observer ) {
+  const [ entry ] = entries;
+  
+  if ( !entry.isIntersecting ) return;
+
+  entry.target.classList.remove( 'section--hidden' );
+  observer.unobserve( entry.target );
+}
+
+const sectionObserver = new IntersectionObserver( revealSection, {
+  root: null,
+  threshold: 0.15,
+} );
+
+allSection.forEach( section => {
+  section.classList.add('section--hidden')
+  sectionObserver.observe( section );
+})
