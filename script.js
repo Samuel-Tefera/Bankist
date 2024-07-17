4e9ad53f024b0a2ef0c88775fe1b376e221e4746
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-17 15:03:55 +0300

    implement sticky navigation bar effect while scrolling down

--- script.js
+++ script.js
@@ -102,4 +102,26 @@ nav.addEventListener( 'mouseover', function ( e ) {
 
 nav.addEventListener( 'mouseout', function ( e ) {
   handleHover( e, 1 );
-})
+} )
+
+// Sticky naviagtion: Intersection Observer API
+const stickyNav = function ( entries ) {
+  const [ entry ] = entries;
+  if ( !entry.isIntersecting ) {
+    nav.classList.add( 'sticky' )
+  }
+  else {
+    nav.classList.remove('sticky')
+  }
+}
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const header = document.querySelector( '.header' );
+const headerObserver = new IntersectionObserver( stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`
+} );
+
+headerObserver.observe( header );
