9d96f319ddc5ac28def121400be879896634259a
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-17 15:03:56 +0300

    add lazy loading effect on images in features section

--- script.js
+++ script.js
@@ -145,4 +145,27 @@ const sectionObserver = new IntersectionObserver( revealSection, {
 allSection.forEach( section => {
   section.classList.add('section--hidden')
   sectionObserver.observe( section );
-})
+} )
+
+// Lazy loading images 
+const imgTargets = document.querySelectorAll( 'img[data-src]' );
+const loadImg = function ( entries, observer ) {
+  const [entry] = entries;
+  if ( !entry.isIntersecting ) return;
+
+  // Replace src with data_src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener( 'load', function () {
+    entry.target.classList.remove( 'lazy-img' );
+  } )
+  observer.unobserve(entry.target)
+}
+
+const imgObserver = new IntersectionObserver( loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px'
+} );
+
+imgTargets.forEach( img => imgObserver.observe( img ) );
